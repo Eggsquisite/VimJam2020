94aed939470a8e0edc8b0516732fc265c97f7ee7
Marc Sevilleja <marc@sevilayha.com>
2020-09-23 01:49:41 -0700

    Added raycast to check for enemy when waypoint reached

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,30 +4,74 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public ClickManager cm;
     private Rigidbody2D rb;
 
-    public float moveSpeed;
     public Vector3 offset;
+    public LayerMask enemyLayer;
+    public float moveSpeed, checkDistance;
 
-    private bool waypointSet = false;
     private GameObject currentWaypoint, setWaypoint;
+    private RaycastHit2D enemyInSight;
+    private bool waypointSet, readyToAttack, facingRight;
+    private float oldPos;
 
     // Start is called before the first frame update
     void Start()
     {
         if (rb == null) rb = GetComponent<Rigidbody2D>();
+        oldPos = transform.position.x;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //if (readyToAttack)
+            CheckEnemy();
 
+        Flip();
+    }
+
+    private void CheckEnemy()
+    {
+        if (facingRight)
+        {
+            enemyInSight = Physics2D.Raycast(transform.position, Vector2.right, checkDistance, enemyLayer);
+            Debug.DrawLine(transform.position, (Vector2)transform.position + Vector2.right * checkDistance, Color.red);
+        }
+        else
+        {
+            enemyInSight = Physics2D.Raycast(transform.position, Vector2.left, checkDistance, enemyLayer);
+            Debug.DrawLine(transform.position, (Vector2)transform.position + Vector2.left * checkDistance, Color.red);
+        }
+
+        if (enemyInSight.collider != null)
+        {
+            Debug.Log("Hit enemy: " + enemyInSight.collider.name);
+        }
+    }
+
+    private void Flip()
+    {
+        if (oldPos < transform.position.x && !facingRight)
+        {
+            // facing/moving right
+            Debug.Log("moving right");
+            facingRight = true;
+            transform.localScale = new Vector3(1, transform.localScale.y, transform.localScale.z);
+        }
+        else if (oldPos > transform.position.x && facingRight)
+        {
+            // facing/moving left
+            Debug.Log("moving left");
+            facingRight = false;
+            transform.localScale = new Vector3(-1, transform.localScale.y, transform.localScale.z);
+        }
+
+        oldPos = transform.position.x;
     }
 
     private void FixedUpdate()
     {
-        //currentWaypoint = cm.GetWaypoint();
         MoveToWaypoint();
     }
 
@@ -42,13 +86,13 @@ public class Player : MonoBehaviour
 
         if (setWaypoint != null)
         {
-            Debug.Log(setWaypoint.name + " Set waypoint");
             Movement();
+            readyToAttack = false;
 
             if (transform.position.x == setWaypoint.transform.position.x)
             {
-                Debug.Log("Waypoint reached");
                 setWaypoint = null;
+                readyToAttack = true;
             }
         }
     }
